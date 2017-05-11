d56d6ed092d0499acc790e7bc1611434ac9071de
Solbon Daribazaron <solbon@gmail.com>
2017-05-11 14:19:28 +0200

    SpringBoot empty project

---
+++ src/main/java/com/example/CourseApiApp.java
@@ -0,0 +1,15 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Solbon on 2017-05-11.
+ */
+@SpringBootApplication
+public class CourseApiApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseApiApp.class, args);
+    }
+}
