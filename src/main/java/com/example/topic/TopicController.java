760e368083c347641619b742a1c19678d10a934c
Solbon Daribazaron <solbon@gmail.com>
2017-05-12 14:33:25 +0200

    Implementing Update and Delete

--- src/main/java/com/example/topic/TopicController.java
+++ src/main/java/com/example/topic/TopicController.java
@@ -30,4 +30,14 @@ public class TopicController {
         topicService.addTopic(topic);
     }
 
+    @RequestMapping(method = RequestMethod.PUT, value = "/topics/{id}")
+    public void updateTopic(@RequestBody Topic topic, @PathVariable String id) {
+        topicService.updateTopic(id, topic);
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE, value = "/topics/{id}")
+    public void deleteTopic(@PathVariable String id) {
+        topicService.deleteTopic(id);
+    }
+
 }
